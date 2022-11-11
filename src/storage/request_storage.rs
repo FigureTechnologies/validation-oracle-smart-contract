e2b668fcf338a66c1adfd623eab4c75a0311afcf
Ravi Patel <rpatel@figure.com>
2022-11-10 19:07:35 -0600

    Support validation request storage searches by owner or status

--- src/storage/request_storage.rs
+++ src/storage/request_storage.rs
@@ -1,27 +1,36 @@
+use std::cmp::Ordering;
+
 use crate::{
     types::{core::error::ContractError, request::validation_request::ValidationRequestOrder},
     util::aliases::ContractResult,
 };
 
-use cosmwasm_std::Storage;
+use cosmwasm_std::{Order, Storage};
 use cw_storage_plus::{Index, IndexList, IndexedMap, MultiIndex};
 use result_extensions::ResultExtensions;
 
-/// The primary key prefix for the storage of [ValidationRequestOrder]s in a [MultiIndex].
+/// The primary key prefix for the storage of [ValidationRequestOrder]s in an [IndexedMap].
 const NAMESPACE_REQUEST_PK: &str = "request";
 /// The prefix in the [RequestIndices] for indexing [ValidationRequestOrder]s by their ID.
-const NAMESPACE_VALIDATION_REQUEST_IDX: &str = "request__request";
+const NAMESPACE_VALIDATION_REQUEST_ID_IDX: &str = "request__id";
+/// The prefix in the [RequestIndices] for indexing [ValidationRequestOrder]s by their owner.
+const NAMESPACE_VALIDATION_REQUEST_OWNER_IDX: &str = "request__owner";
+/// The prefix in the [RequestIndices] for indexing [ValidationRequestOrder]s by their status.
+const NAMESPACE_VALIDATION_REQUEST_STATUS_IDX: &str = "request__status";
 
 /// Defines a collection of [MultiIndex]s for storing [ValidationRequestOrder]s in
 /// a shared [primary key namespace](NAMESPACE_REQUEST_PK).
 pub struct RequestIndices<'a> {
-    pub request_index: MultiIndex<'a, String, ValidationRequestOrder, String>,
+    pub id_index: MultiIndex<'a, String, ValidationRequestOrder, String>,
+    pub owner_index: MultiIndex<'a, String, ValidationRequestOrder, String>,
+    pub status_index: MultiIndex<'a, String, ValidationRequestOrder, String>,
 }
 impl<'a> IndexList<ValidationRequestOrder> for RequestIndices<'a> {
     fn get_indexes(
         &'_ self,
     ) -> Box<dyn Iterator<Item = &'_ dyn Index<ValidationRequestOrder>> + '_> {
-        let v: Vec<&dyn Index<ValidationRequestOrder>> = vec![&self.request_index];
+        let v: Vec<&dyn Index<ValidationRequestOrder>> =
+            vec![&self.id_index, &self.owner_index, &self.status_index];
         Box::new(v.into_iter())
     }
 }
@@ -29,10 +38,20 @@ impl<'a> IndexList<ValidationRequestOrder> for RequestIndices<'a> {
 /// Returns the contract's storage of validation requests.
 pub fn requests<'a>() -> IndexedMap<'a, &'a [u8], ValidationRequestOrder, RequestIndices<'a>> {
     let indices = RequestIndices {
-        request_index: MultiIndex::new(
+        id_index: MultiIndex::new(
             |_pk, request: &ValidationRequestOrder| request.id.clone(),
             NAMESPACE_REQUEST_PK,
-            NAMESPACE_VALIDATION_REQUEST_IDX,
+            NAMESPACE_VALIDATION_REQUEST_ID_IDX,
+        ),
+        owner_index: MultiIndex::new(
+            |_pk, request: &ValidationRequestOrder| request.owner.clone().to_string(),
+            NAMESPACE_REQUEST_PK,
+            NAMESPACE_VALIDATION_REQUEST_OWNER_IDX,
+        ),
+        status_index: MultiIndex::new(
+            |_pk, request: &ValidationRequestOrder| request.status.to_string(),
+            NAMESPACE_REQUEST_PK,
+            NAMESPACE_VALIDATION_REQUEST_STATUS_IDX,
         ),
     };
     IndexedMap::new(NAMESPACE_REQUEST_PK, indices)
@@ -83,7 +102,7 @@ pub fn update_request(
     }
 }
 
-/// Stores a validation request into the contract's storage, overwriting
+/// Inserts a validation request into the contract's storage, overwriting
 /// any existing validation request with the same ID.
 ///
 /// # Parameters
@@ -137,20 +156,63 @@ pub fn get_request_by_id<S: Into<String>>(
         })
 }
 
-// TODO: Adapt this into functions for retrieving requests by fields other than the ID
-// pub fn get_requests_by_collateral_id<S: Into<String>>(
-//     storage: &dyn Storage,
-//     collateral_id: S,
-// ) -> Vec<ValidationRequestOrder> {
-//     requests()
-//         .idx
-//         .collateral_index
-//         .prefix(collateral_id.into())
-//         .range(storage, None, None, DEFAULT_SEARCH_ORDER)
-//         .filter(|result| result.is_ok())
-//         .map(|result| result.unwrap().1)
-//         .collect()
-// }
+/// Finds all validation requests made by a given Provenance address.
+///
+/// # Parameters
+///
+/// * `storage` A reference to the contract's internal storage.
+/// * `owner` The bech32 Provenance address of a requestor.
+/// * `compare` An optional function to sort the results by.
+pub fn get_requests_by_owner<
+    S: Into<String>,
+    C: FnMut(&ValidationRequestOrder, &ValidationRequestOrder) -> Ordering,
+>(
+    storage: &dyn Storage,
+    owner: S,
+    compare: Option<C>,
+) -> Vec<ValidationRequestOrder> {
+    let mut requests: Vec<ValidationRequestOrder> = requests()
+        .idx
+        .owner_index
+        .prefix(owner.into())
+        .range(storage, None, None, Order::Ascending)
+        .filter(|result| result.is_ok())
+        .map(|result| result.unwrap().1)
+        .collect();
+    if let Some(comparator) = compare {
+        requests.sort_by(comparator);
+    }
+    requests
+}
+
+/// Finds all validation requests with a given status.
+///
+/// # Parameters
+///
+/// * `storage` A reference to the contract's internal storage.
+/// * `status` A validation request status.
+/// * `compare` An optional function to sort the results by.
+pub fn get_requests_by_status<
+    S: Into<String>,
+    C: FnMut(&ValidationRequestOrder, &ValidationRequestOrder) -> Ordering,
+>(
+    storage: &dyn Storage,
+    status: S,
+    compare: Option<C>,
+) -> Vec<ValidationRequestOrder> {
+    let mut requests: Vec<ValidationRequestOrder> = requests()
+        .idx
+        .status_index
+        .prefix(status.into())
+        .range(storage, None, None, Order::Ascending)
+        .filter(|result| result.is_ok())
+        .map(|result| result.unwrap().1)
+        .collect();
+    if let Some(comparator) = compare {
+        requests.sort_by(comparator);
+    }
+    requests
+}
 
 /// Deletes a validation request by its ID, returning a [Result]
 /// reflecting whether a matching request was found or not.
